b8da262bbe09db399eec596145fe6f009696107a
Ubuntu <ubuntu@ip-172-31-42-45.us-east-2.compute.internal>
2024-03-10 19:40:27 +0000

    shell: for loyltytest bash script

---
+++ nibba_nibbi_loyalty_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+<< comment
+loyalty test for nibba nibi 
+with arguments veriable
+comment
+
+echo "if nibba will see to nibbi then he will turn"
+
+read girl
+
+if [ $girl == "nibbi" ]; then
+
+   echo "loyal"
+   echo "niibbi will make a folder"
+   mkdir nibbloyaltestpass
+
+else
+   echo "nibaa move away"
+
+fi
