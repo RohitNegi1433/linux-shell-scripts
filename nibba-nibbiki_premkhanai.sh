5bbdbd507a136891b57227997c92eb4094939c5b
Ubuntu <ubuntu@ip-172-31-42-45.us-east-2.compute.internal>
2024-03-10 19:39:45 +0000

    sheel: added premkhani script

---
+++ nibba-nibbiki_premkhanai.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+#Nibba nibbi ki ajab gazab prem kahani
+
+echo "Hello Nibbi"
+
+echo "hi nibba"
+
+
+echo "nibba - can we got out for coffe"
+
+date
+
+echo "nibbi = yes sure"
+
+echo "nibba =i am waiting for you in coffe table"
+
+uptime
+
+echo " still i am waiting "
+
+echo " nibbi = i am doing some important work creating files in desktop"
+
+ls /home/ubuntu
+
+echo "i willcreate files in your desktop"
+mkdir nibbanibbi
+
+echo " nibba = where you are busy"
+echo "i am busy with somone"
+read  someone
+echo  $someone
+
+echo "but $someone you are busy with $1"
+echo " not iam not its less $#"
+
+echo $@
